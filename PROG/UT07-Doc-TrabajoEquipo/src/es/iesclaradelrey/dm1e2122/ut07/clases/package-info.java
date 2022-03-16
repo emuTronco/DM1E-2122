4ee2c4b24c51edb367825ddf8f2f9ab1c113ff87
José Luis López Alvarez <jllopezalvarez@educa.madrid.org>
2022-03-16 10:12:40 -0400

    Añadido código de la clase comentada con JavaDoc. Se ha excluido (ignorado) del repositorio la carpeta "doc" del proyecto, porque no es necesaria, ya que la documentación se puede regenerar siempre que se quiera.

---
+++ PROG/UT07-Doc-TrabajoEquipo/src/es/iesclaradelrey/dm1e2122/ut07/clases/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Paquete de clases del proyecto.
+ * <p>
+ * Paquete para demostrar la documentación de clases con JavaDoc.
+ * <p>
+ * Los comentarios JavaDoc para paquetes tienen que colocarse en un fichero "package-info.java".
+ * <p>
+ * Todos los comentarios pueden incluir HTML.
+ * <p>
+ * Elicpse, al crear un nuevo paquete, nos da la opción de crear este fichero.
+ */
+package es.iesclaradelrey.dm1e2122.ut07.clases;
